c24bc1c42a5dd6818b09f780c6e1dbc685c6fc9d
glyn <glyn@users.noreply.github.com>
2025-12-04 10:42:50 +0000

    deploy: ddcbfe2d91a99f498dacae75f57a85b745ef9571

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-416f1865.js',
+                path_to_root + 'searchindex-c88ce482.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
